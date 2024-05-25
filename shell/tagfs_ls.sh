e3eba0d89915d4d3547f9e34d48598e5cb2f0fed
Raghu Boggavarapu <rteja.bits@gmail.com>
2024-05-25 08:06:06 +0530

    some display fixes in tagfs_ls

--- shell/tagfs_ls.sh
+++ shell/tagfs_ls.sh
@@ -2,14 +2,15 @@
 
 files=$(ls)
 
-tagbounday=$(tagfs getboundary >> /dev/null 2>> /dev/null)
+tagfs getboundary >> /dev/null 2>> /dev/null
 retval=$?
-
 
 for f in $files
 do
     if [ $retval -ne 1 ]; then
         filetags=$(tagfs getresourcetags "$f" 2>>/dev/null)
     fi
-    echo "$f" "::" "$filetags"
+    # shellcheck disable=SC2048
+    # shellcheck disable=SC2116
+    echo "$f" "$(printf '\t')" "::" $(echo "$filetags")
 done
