71c070b093870b1c83783e3578cdc17d4a94c3cf
Raghu Boggavarapu <rteja.bits@gmail.com>
2024-06-02 12:00:21 +0530

    Making the prompt script a little more sophisticated in case the directory upwards does not return any tags, then files in the directory are checked to get some tags - this still needs more thought and wip

--- shell/tagfs_prompt.sh
+++ shell/tagfs_prompt.sh
@@ -5,25 +5,36 @@ get_abs_path() {
   echo "$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
 }
 
+subst_comma_for_space() {
+    args=("$@")
+    echo "$args" | sed 's/[[:space:]]\+/,/g' | sed -e 's/,/, /g'
+}
+
+get_dir_tags() {
+    cdirtags=$(tagfs getresourcetags "$1" 2>>/dev/null);
+    retval=$?
+    if [ $retval -ne 1 ]; then
+        # shellcheck disable=SC2116
+        # shellcheck disable=SC2086
+        #output="{$(echo $cdirtags | sed 's/[[:space:]]\+/,/g' | sed -e 's/,/, /g')}"
+        output="{$(subst_comma_for_space $cdirtags)}"
+    else
+        output='{}'
+    fi
+    echo "$output"
+}
+
 bdry=$(tagfs getboundary 2>>/dev/null)
 retval=$?
 output='';
+# try moving up to see if we get any folder with tags
 if [ $retval -ne 1 ]; then
     cdir=$(pwd)
     bdrdir=$(get_abs_path "$bdry")
 
     while true; do
         curdir=$(get_abs_path "$cdir")
-
-        cdirtags=$(tagfs getresourcetags "$curdir" 2>>/dev/null);
-        retval=$?
-        if [ $retval -ne 1 ]; then
-            # shellcheck disable=SC2116
-            # shellcheck disable=SC2086
-            output="{$(echo $cdirtags | sed 's/[[:space:]]\+/,/g' | sed -e 's/,/, /g')}"
-        else
-            output='{}'
-        fi
+        output=$(get_dir_tags "$curdir")
 
         # we already found a subfolder that has tags
         if [ "$output" != "{}" ]; then
@@ -37,6 +48,25 @@ if [ $retval -ne 1 ]; then
 
         cdir=$(dirname "$cdir")
     done
+fi
+
+# try looking at all files and see if any of them has tags
+# need to make this logic more smarter by merging some flags based on tag graph
+if [ "$output" == "{}" ]; then
+    output=''
+    files=$(ls)
+    if [ $retval -ne 1 ]; then
+        for f in $files
+        do
+            filetags=$(tagfs getresourcetags "$f" 2>>/dev/null)            
+            # shellcheck disable=SC2086
+            # shellcheck disable=SC2116
+            output=$(echo $output $filetags)
+        done
+    fi
+    # shellcheck disable=SC2178
+    output=$(echo "$output" | sort | uniq)
+    output="{$(subst_comma_for_space "$output")}"
 fi
 echo "$output"
 
