8d852fc8007134f7a67a50f30f6cb573c0339515
Raghu Boggavarapu <rteja.bits@gmail.com>
2024-06-02 11:58:02 +0530

    Disable shellcheck warnings in bash completion script

--- shell/tagfs_completion.bash
+++ shell/tagfs_completion.bash
@@ -3,52 +3,64 @@
 _tagfs_completions()
 {
   if [ "${#COMP_WORDS[@]}" == "2" ]; then
+    # shellcheck disable=SC2207
     COMPREPLY=($(compgen -W "init getboundary lstags addtags renametag linktags addresource tagresource untagresource lsresources rmresource mvresource getresourcetags rmresourcetags help" "${COMP_WORDS[1]}"))
   fi
 
   if [ "${#COMP_WORDS[@]}" == "3" ]; then
 
     if [ "${COMP_WORDS[1]}" == "addresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "tagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "untagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "mvresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
     
     if [ "${COMP_WORDS[1]}" == "rmresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "getresourcetags" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "rmresourcetags" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -f -- "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "lstags" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "lsresources" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[2]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "renametag" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[2]}"))
     fi
     
     if [ "${COMP_WORDS[1]}" == "linktags" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[2]}"))
     fi
     
@@ -56,24 +68,29 @@ _tagfs_completions()
 
   if [ "${#COMP_WORDS[@]}" == "4" ]; then
     if [ "${COMP_WORDS[1]}" == "tagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[3]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "untagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[3]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "linktags" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[3]}"))
     fi
   fi
 
   if [ ${#COMP_WORDS[@]} -gt 4 ]; then
     if [ "${COMP_WORDS[1]}" == "tagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[${#COMP_WORDS[@]} - 1]}"))
     fi
 
     if [ "${COMP_WORDS[1]}" == "untagresource" ]; then
+      # shellcheck disable=SC2207
       COMPREPLY=($(compgen -W "$(tagfs lstags)" "${COMP_WORDS[${#COMP_WORDS[@]} - 1]}"))
     fi
   fi
