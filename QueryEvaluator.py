b11d9e7629d265d28857661a493333bbf9764586
Raghu Teja Boggavarapu <rteja.bits@gmail.com>
2025-04-15 18:58:11 +0530

    Readability, Robustness and Maintenance improvements to QueryEvaluator code

--- QueryEvaluator.py
+++ QueryEvaluator.py
@@ -1,23 +1,31 @@
 import TagHandler
 import re
+from functools import lru_cache
+
 
 # Implementation of shuning yard algorithm
-class QueryEvaluator :
+class QueryEvaluator:
+    OPERATORS = {'|': 0, '~': 0, '&': 1}
+    VALID_OPERATORS = set(['|', '&', '~'])
+    LEFT_PAREN = '('
+    RIGHT_PAREN = ')'
+
     def __init__(self, th: TagHandler.TagHandler) :
         self.th = th
         self.operators = []
         self.values = []
         self.fullresources = []
 
-    def tokenize(self, querystr):
+    def tokenize(self, querystr: str) -> list[str] :
         tokens = re.findall("[()&|~]|[a-z0-9]+", querystr)
         return tokens
 
-    def peek(self):
+    def peek(self) :
         return self.operators[-1] if self.operators else None
 
     def apply_operator(self) :
         operator = self.operators.pop()
+        assert(operator in QueryEvaluator.VALID_OPERATORS)
         if operator == "&" :
             v1 = self.values.pop()
             v2 = self.values.pop()
@@ -31,36 +39,43 @@ class QueryEvaluator :
             if len(self.fullresources) == 0 :
                 self.fullresources = set(self.th.get_resource_ids())
             self.values.append(self.fullresources.difference(v1))
-        else :
-            print("invalid operator")
-            exit(1)
 
-    def greater_precedence(self, op1, op2):
-        precedences = {"|": 0, "~": 0, "&": 1}
-        return precedences[op1] > precedences[op2]
+    def greater_precedence(self, op1, op2) -> bool:
+        return QueryEvaluator.OPERATORS[op1] > QueryEvaluator.OPERATORS[op2]
+
+    def validate_expression(self, expression: str) -> bool:
+        """Validates query expression for balanced parentheses and valid operators"""
+        stack = []
+        for char in expression:
+            if char == self.LEFT_PAREN:
+                stack.append(char)
+            elif char == self.RIGHT_PAREN:
+                if not stack:
+                    return False
+                stack.pop()
+        return len(stack) == 0
 
     def evaluate_query(self, expression):
+        assert(self.validate_expression(expression))
         tokens = self.tokenize(expression)
         self.values = []
         self.operators = []
         for token in tokens:
-            if token == "(":
+            if token == QueryEvaluator.LEFT_PAREN:
                 self.operators.append(token)
-            elif token == ")":
+            elif token == QueryEvaluator.RIGHT_PAREN:
                 top = self.peek()
-                while top is not None and top != "(":
+                while top is not None and top != QueryEvaluator.LEFT_PAREN:
                     self.apply_operator()
                     top = self.peek()
                 self.operators.pop()  # Discard the '('
-            elif token[0] not in ['|','&','~']:
-                tag_closure = self.th.get_tag_closure([token])
-                tag_closure_ids = list(map(self.th.get_tag_id, tag_closure))
-                r = set(self.th.get_resources_by_tag_id(tag_closure_ids))                
-                self.values.append(r)
+            elif token[0] not in QueryEvaluator.VALID_OPERATORS:
+                r = self.get_tag_closure(token)
+                self.values.append(r)            
             else:
                 # Operator
                 top = self.peek()
-                while top is not None and top != "(" and self.greater_precedence(top, token):
+                while top is not None and top != QueryEvaluator.LEFT_PAREN and self.greater_precedence(top, token):
                     self.apply_operator()
                     top = self.peek()
                 self.operators.append(token)
@@ -68,3 +83,9 @@ class QueryEvaluator :
             self.apply_operator()
         res = list(map(self.th.get_resource_url, self.values[0]))
         return res
+    
+    @lru_cache(maxsize=128)
+    def get_tag_closure(self, tag: str) -> set[str]:
+        tag_closure = self.th.get_tag_closure([tag])
+        tag_closure_ids = list(map(self.th.get_tag_id, tag_closure))
+        return set(self.th.get_resources_by_tag_id(tag_closure_ids))
